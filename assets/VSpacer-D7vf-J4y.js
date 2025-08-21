ef813bebb55a21f919ca29bbfdafe83b2a93adf2
margo-lee <margo.lee@hyweb.com.tw>
2025-08-21 17:21:31 +0800

    deploy

--- assets/VSpacer-D7vf-J4y.js
+++
@@ -1,1 +0,0 @@
-import"./VRow-Sh-SkzNy.js";import{g as r}from"./VCard-Czyu9MD9.js";const p=r("v-spacer","div","VSpacer");export{p as V};
---
+++ assets/scopeId-BUwIHqT2.js
@@ -0,0 +1,1 @@
+import{ah as o}from"./index-Cp5Gxez6.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
